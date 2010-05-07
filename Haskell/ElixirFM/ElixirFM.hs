ee33b670d0373738f409cefc6291bf94dacda0e4
Otakar Smrz <otakar-smrz@users.sf.net>
2010-05-07 23:31:50 +0000

    New release ...
    --HG--
    extra : convert_revision : svn%3A1d015aef-350d-0410-9c20-f207462eb880%40951

--- Haskell/ElixirFM/ElixirFM.hs
+++ Haskell/ElixirFM/ElixirFM.hs
@@ -88,6 +88,6 @@ import Prelude hiding (lookup)
 
 import Version
 
-version = Version [1, 1, max build 950] []
+version = Version [1, 1, max build 951] []
 
     where Version [build] [] = revised "$Revision$"
