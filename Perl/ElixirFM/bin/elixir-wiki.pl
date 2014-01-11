d1dcf40377d2820412371d53600c4eff374e068c
Otakar Smrz <otakar-smrz@users.sf.net>
2014-01-11 02:20:55 +0100

    Unpretty parsing

--- Perl/ElixirFM/bin/elixir-wiki.pl
+++ Perl/ElixirFM/bin/elixir-wiki.pl
@@ -4,8 +4,9 @@ use ElixirFM;
 use ElixirFM::Exec;
 
 use Data::Dumper;
+use JSON;
 
-$\ = "\n";
+# $\ = "\n";
 
 # print map { "$_\n" } ElixirFM::retrieve "perfect active third indicative";
 
@@ -14,32 +15,23 @@ $\ = "\n";
 # print join " ", ElixirFM::retrieve "(verb act sg pl) (noun adj sg nom indef) S V[PI]-A";
 
 # print ElixirFM::describe "V[PI]-A-3[FM]S--";
-
 # print ElixirFM::describe "[NA]------S1I", 'terse';
 
 # $r = ElixirFM::Exec::elixir "resolve", ["tim"], "lasaEat lasaEat\nlasaEat";
-#    # ElixirFM::Exec::elixir "resolve", "حوله";
+# $r = ElixirFM::Exec::elixir "resolve", "حوله";
 
-# @u = ElixirFM::unpretty $r;
+# $r = ElixirFM::Exec::elixir "inflect", ["VCJ----S--"], "(100,[])\n\n(100,[])\n(100,[])";
+# $r = ElixirFM::Exec::elixir "inflect", ["VCJ----S--"], "حول";
 
-# print Data::Dumper->Dump([\@u], ["*u"]);
+# $r = ElixirFM::Exec::elixir "derive", ["[VNA]---------"], "(100,[])\n\n(100,[])\n(100,[])";
 
-$r = ElixirFM::Exec::elixir "inflect", ["VCJ----S--"], "(100,[])\n\n(100,[])\n(100,[])";
-   # ElixirFM::Exec::elixir "inflect", ["VCJ----S--"], "حول";
+# $r = ElixirFM::Exec::elixir "lookup", "حول";
+# $r = ElixirFM::Exec::elixir "lookup", "twitter";
+
+$r = ElixirFM::Exec::elixir "lookup", "ten", "hundred", "twelve", "he", "which";
 
 @u = ElixirFM::unpretty $r;
 
 print Data::Dumper->Dump([\@u], ["*u"]);
 
-# $r = ElixirFM::Exec::elixir "derive", ["[VNA]---------"], "(100,[])\n\n(100,[])\n(100,[])";
-
-# @u = ElixirFM::unpretty $r;
-
-# print Data::Dumper->Dump([\@u], ["*u"]);
-
-# $r = ElixirFM::Exec::elixir "lookup", "حول";
-#    # ElixirFM::Exec::elixir "lookup", "twitter";
-
-# @u = ElixirFM::unpretty $r;
-
-# print Data::Dumper->Dump([\@u], ["*u"]);
+print JSON->new()->allow_nonref()->encode(\@u);
--- Python/elixir.py
+++ Python/elixir.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import subprocess
+import json
+import re
 import sys
 
 def elixir(mode, text, *opts):
@@ -20,8 +22,29 @@ def derive(text, *opts):
 def lookup(text, *opts):
     return elixir('lookup', text, *opts)
 
+def unpretty(data):
+    return [ groups([ item.split('\t') for word in unwords(line)
+                                       for item in word.splitlines() ])
+             for line in unlines(data) ]
+
+def unlines(data):
+    data = re.split('(?:(?<=\n)\n|(?<=^)\n)', data)
+    return data[:-1] if data and data[-1] == "" else data
+
+def unwords(data):
+    return [ word for line in unlines(data)
+                  for word in re.split('(?:(?<=\n)\n|(?<=^)\n)', line) ]
+
+def groups(data):
+    return data
+
 def main(*args):
     sys.stdout.write(elixir(args[0] if args else "", sys.stdin.read(), *args[1:]))
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    if sys.argv[1] == "--":
+        data = sys.stdin.read()
+        data = unpretty(data)
+        print json.dumps(data)
+    else:
+        main(*sys.argv[1:])
