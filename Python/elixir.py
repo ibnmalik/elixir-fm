85289a3da41506f9509f142946381bd8548bec3d
Otakar Smrz <otakar-smrz@users.sf.net>
2014-01-12 03:57:44 +0100

    Unpretty parsing

--- Python/elixir.py
+++ Python/elixir.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+"""elixir.py: Interfaces to the ElixirFM system implementing Functional Arabic Morphology"""
+
+__version__   = "1.2.1"
+__license__   = "GPL"
+__copyright__ = "2014"
+__author__    = "Otakar Smrz"
+__contact__   = "otakar-smrz users.sf.net"
+__homepage__  = "http://elixir-fm.sf.net/"
+
 import subprocess
 import json
 import re
+import itertools
 import sys
 
 def elixir(mode, text, *opts):
@@ -36,15 +46,28 @@ def unwords(data):
                   for word in re.split('(?:(?<=\n)\n|(?<=^)\n)', line) ]
 
 def groups(data):
-    return data
+    tree = [[]]
+    for i in range(len(data)):
+        twig = list(itertools.dropwhile(lambda x: not x.split(), data[i]))
+        if not twig:
+            continue
+        nest = len(data[i]) - len(twig) - len(tree) + 1
+        twig = reduce(lambda x, y: [y, x], reversed(twig[:-1]), [twig[-1]])
+        if nest < 0:
+            tree = tree[:nest]
+        elif nest > 0:
+            for n in range(nest):
+                tree.append(tree[-1][-1])
+        tree[-1].append(twig)
+    return tree[0]
 
 def main(*args):
     sys.stdout.write(elixir(args[0] if args else "", sys.stdin.read(), *args[1:]))
 
 if __name__ == "__main__":
-    if sys.argv[1] == "--":
-        data = sys.stdin.read()
-        data = unpretty(data)
-        print json.dumps(data)
+    if len(sys.argv) > 1 and sys.argv[1] == "unpretty":
+        data = unpretty(sys.stdin.read())
+        nest = int(sys.argv[2]) if len(sys.argv) > 2 and re.search("^[0-9]+$", sys.argv[2]) else None
+        print json.dumps(data, indent=nest)
     else:
         main(*sys.argv[1:])
